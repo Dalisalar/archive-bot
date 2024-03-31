49ea0094ec1681a07f3279bee3f9404bc469eb33
Dalisalar <timurgan.gi2@gmail.com>
2024-03-31 23:49:30 +0300

    Initital commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from motor.motor_asyncio import AsyncIOMotorClient
+
+TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+MONGO_URI = os.environ.get('TELEGRAM_BOT_TOKEN', 'mongodb://mongo:27017')
+
+# Initialize aiogram bot
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+dp = Dispatcher(bot)
+
+# Initialize MongoDB
+client = AsyncIOMotorClient(MONGO_URI)
+db = client.get_database("telegram_bot")
+messages_collection = db.get_collection("messages")
+
+# Message handler
+@dp.message()
+async def save_message(message: types.Message):
+    await messages_collection.insert_one(message.model_dump())
+
+if __name__ == '__main__':
+    asyncio.run(dp.start_polling(polling_timeout=60))
